fcb3c2cd4c3e44e3b445b6cec28ea816ba243d20
ajpetrosky <ajpetrosky.dev@gmail.com>
2018-04-26 14:30:49 -0400

    Add gitignore

---
+++ src/json_parser.py
@@ -0,0 +1,14 @@
+# NBA Game Predictor
+# File: json_parser.py
+# Authors: Andrew Petrosky & Tarmily Wen
+#
+# Provides a function which takes in a season string,
+# loads the appropriate games from the season, and returns
+# two lists of dictionaries representing games in date
+# order, one of which is regular season and the other
+# is playoffs
+
+import json
+
+def parse_season(season):
+        
