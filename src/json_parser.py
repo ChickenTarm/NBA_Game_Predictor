a3b034b3979aeef52199eee7d46791e9896de598
ajpetrosky <ajpetrosky.dev@gmail.com>
2018-04-26 15:03:28 -0400

    Add json data parser and removed old data

--- src/json_parser.py
+++ src/json_parser.py
@@ -4,11 +4,27 @@
 #
 # Provides a function which takes in a season string,
 # loads the appropriate games from the season, and returns
-# two lists of dictionaries representing games in date
-# order, one of which is regular season and the other
+# a tuple of two lists of dictionaries representing games in
+# date order, one of which is regular season and the other
 # is playoffs
 
 import json
+import os
+
+
+def parse(season, season_type):
+    games = []
+    prefix = "../data/nba/" + season + "/" + season_type + "/"
+    for game_file in os.listdir(prefix):
+        game_file = os.fsdecode(game_file)
+        with open(prefix + game_file) as game:
+            game_json = game.read()
+            game_data = json.loads(game_json)
+            games += [game_data]
+    return games
+
 
 def parse_season(season):
-        
+    regular_season = parse(season, "regular")
+    post_season = parse(season, "playoffs")
+    return regular_season, post_season
